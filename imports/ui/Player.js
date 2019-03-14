054289e10886331160df2227caeac54da2a56fea
Bálint Budavölgyi <bala92x@gmail.com>
2019-03-14 09:34:40 +0100

    Initial commit

---
+++ imports/ui/Player.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Players } from './../api/players';
+
+export default class Player extends React.Component {
+    render() {
+        const itemClassName = `item item--position-${this.props.player.rank}`;
+        const hasSingularPoint = Math.abs(this.props.player.score) == 1;
+        return (
+            <div key={this.props.player._id} className={itemClassName}>
+                <div className="player">
+                    <div>
+                        <h3 className="player__name">
+                            {this.props.player.name}
+                        </h3>
+                        <p className="player__stats">
+                            {this.props.player.position} place: {this.props.player.score} point{hasSingularPoint ? '' : 's'}.
+                        </p>
+                    </div>
+                    <div className="player__actions">
+                        <button className="button button--round" onClick={() => {
+                            Players.update(this.props.player._id, {
+                                $inc: { score: -1 }
+                            })
+                        }}>
+                            -1
+                        </button>
+                        <button className="button button--round" onClick={() => {
+                            Players.update(this.props.player._id, {
+                                $inc: { score: 1 }
+                            })
+                        }}>
+                            +1
+                        </button>
+                        <button className="button button--round" onClick={() => Players.remove(this.props.player._id)}>X</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
+
+Player.propTypes = {
+    player: PropTypes.object.isRequired
+};
